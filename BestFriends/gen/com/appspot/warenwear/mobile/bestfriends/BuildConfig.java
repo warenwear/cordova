24e41f83194e3fdf227c9f3865f538db9197330f
htmlpress <egalian@hotmail.com>
2015-10-04 16:36:32 +1000

    directories created

---
+++ BestFriends/gen/com/appspot/warenwear/mobile/bestfriends/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.appspot.warenwear.mobile.bestfriends;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
